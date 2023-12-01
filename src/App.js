d1fa16c32af7e1870da07c6c5190be2fc633846a
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-01 16:21:21 +0100

    challenge solved

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </div>
+  );
 }
+
+const Smiley = ({ isHappy }) => (
+  <span className="smiley">{isHappy ? "😁" : "😢"}</span>
+);
